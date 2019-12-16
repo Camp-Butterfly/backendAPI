c79592feac6d6fa720ce7304b9a51fa559bd662c
Stephen K <stephenjk@optonline.net>
2019-12-16 18:49:40 -0500

    add parsing for likely result

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 from gevent.pywsgi import WSGIServer
 import grpc
@@ -6,42 +7,44 @@ import numpy as np
 import requests
 import tensorflow as tf
 import os
+import base64
+import io
+import PIL
+import json
 
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
 from tensorflow.keras.preprocessing import image
-
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-
-	# Download image 
-	img = image.load_img("cabbage.jpg", target_size=(150,150))
-	# map to tensor array
+@app.route('/api/v1/model', methods=['POST'])
+def image_post():
+	test = request.get_json(force=True)
+	print(test)
+	img_c = test['image_content']
+	img_c = base64.b64decode(img_c)
+	buf = io.BytesIO(img_c)
+	img = Image.open(buf)
+	img = img.resize([150,150])
 	img_tensor = image.img_to_array(img)
-	# Assign dimensions
 	img_tensor = np.expand_dims(img_tensor, axis=0)
-	# pass into workable variable
 	data = img_tensor
-	# mapped image
-	#print(data)  
-	  
-	# establish channel to docker image container
+
 	channel = grpc.insecure_channel('192.168.99.100:8500')
-  	grpc.channel_ready_future(channel).result()
 	# create variable for service that sends object to channel
 	stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 	# assign values to props of request
-	request = predict_pb2.PredictRequest()
-	request.model_spec.name = 'testmodel'
-	request.model_spec.signature_name = 'serving_default'
-	request.inputs['input_image'].CopyFrom(
+	req = predict_pb2.PredictRequest()
+	req.model_spec.name = 'testmodel'
+	req.model_spec.signature_name = 'serving_default'
+	req.inputs['input_image'].CopyFrom(
 	tf.make_tensor_proto(data,shape=[1,150,150,3])
 	)
+
 	#send request to docker image container
-	result = stub.Predict(request,10.0)
+	result = stub.Predict(req,10.0)
 	# response from model
 	floats = np.array(list(result.outputs['dense_1/Softmax:0'].float_val)) 
   	max_ = floats.argmax()
@@ -50,7 +53,8 @@ def main():
   	print("\n")
   	print(max_)
   	print("\n")
-  	return max_
+  	res = json.dumps(max_)
+	return res
 
 @app.after_request
 def after_request(response):
@@ -59,7 +63,7 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
-if __name__=='__main__':
-	#app.run()
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+#if __name__=='__main__':
+app.run()
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
