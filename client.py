66838b449e5409d7e8e0d63a917f68f95e655655
Stephen K <stephenjk@optonline.net>
2019-12-16 23:40:57 -0500

    update CORS policy

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_cors import cross_origin
 
 from gevent.pywsgi import WSGIServer
